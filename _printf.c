f9aad3cb054bd3c6f734a4378f830cb7af3fd862
EmanNeyaz <emanneyaz0@gmail.com>
2023-10-17 11:21:24 -0700

    L

---
+++ _printf.c
@@ -0,0 +1,82 @@
+#include "main.h"
+
+/**
+ * _printf_char - Print a character.
+ * @c: The character to print.
+ *
+ * Return: The number of characters printed.
+ */
+int _printf_char(char c)
+{
+write(1, &c, 1);
+return (1);
+}
+
+/**
+ * _printf_string - Print a string.
+ * @s: The string to print.
+ *
+ * Return: The number of characters printed.
+ */
+int _printf_string(char *s)
+{
+int characters_printed = 0;
+if (s == NULL)
+{
+characters_printed += write(1, "(null)", 6);
+}
+else
+{
+characters_printed += write(1, s, strlen(s));
+}
+return (characters_printed);
+}
+
+/**
+ * _printf - Printf function.
+ * @format: The format string.
+ * @...: Variable arguments.
+ *
+ * Return: The number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+int characters_printed = 0;
+va_list list;
+va_start(list, format);
+while (*format)
+{
+if (*format != '%')
+{
+characters_printed += _printf_char(*format);
+}
+else
+{
+format++;
+switch (*format)
+{
+case '%':
+characters_printed += _printf_char('%');
+break;
+case 'c':
+characters_printed += _printf_char(va_arg(list, int));
+break;
+case 's':
+characters_printed += _printf_string(va_arg(list, char *));
+break;
+case 'd':
+characters_printed += printf("%d", va_arg(list, int));
+break;
+case 'i':
+characters_printed += printf("%i", va_arg(list, int));
+break;
+default:
+va_end(list);
+return (0);
+}
+}
+format++;
+}
+va_end(list);
+return (characters_printed);
+}
---
+++ main.h
@@ -0,0 +1,42 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
+
+
+
+/*Mandatory functions*/
+int _printf(const char *format, ...);
+int op_ch(va_list arg);
+int op_st(va_list arg);
+int op_nu(va_list arg);
+
+/*Advanced functions*/
+int op_fl(va_list arg);
+int op_bi(va_list arg);
+int _rot13(va_list agr);
+
+/*Other useful functions*/
+int count(const char *str);
+int _putchar(char c);
+void print_number(int n);
+
+/**
+* struct specifier - structure
+* @ch: character with the specifier
+* @f: Function associated
+*
+* Description: Define a new data type specifier that
+* select the function to execute for the specifier selected.
+*/
+typedef struct specifier
+{
+	char *ch;
+	int (*f)(va_list str);
+} specifier;
+
+#endif
