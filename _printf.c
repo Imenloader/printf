67acc725eeb994ed7f174bdbd77e73a3723ec20e
Imenloader <greenfeeda@gmail.com>
2023-10-17 19:59:15 -0700

    last minute fix

--- _printf.c
+++ _printf.c
@@ -66,10 +66,10 @@ case 's':
 characters_printed += _printf_string(va_arg(list, char *));
 break;
 case 'd':
-characters_printed += _printf("%d", va_arg(list, int));
+characters_printed += printf("%d", va_arg(list, int));
 break;
 case 'i':
-characters_printed += _printf("%i", va_arg(list, int));
+characters_printed += printf("%i", va_arg(list, int));
 break;
 default:
 va_end(list);
